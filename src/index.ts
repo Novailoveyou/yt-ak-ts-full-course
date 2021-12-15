061083fba43bfb6e641fa5bdcfc0eba37052c4b5
novailoveyou <novailoveyoux@gmail.com>
2021-12-15 20:42:29 +0300

    type aliases

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+type ID = string
+type PopularTag = string
+
 interface UserInterface {
+  id: ID
   name: string
   surname: string
 }
+
+const popularTags: PopularTag[] = ['dragon', 'coffee']
 
 let username: string = 'alex'
 
