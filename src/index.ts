86e784d29e8b53aa1a230f2a0c0b24a6b92bf275
novailoveyou <novailoveyoux@gmail.com>
2021-12-16 00:49:09 +0300

    type unknown

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-const doSomething = (): never => {
-  throw 'never'
-}
+let vAny: any = 10
+let vUnknown: unknown = 10
 
-let foo: any = undefined
-foo = 'foo'
+let s1: string = vAny
+let s2: string = vUnknown
